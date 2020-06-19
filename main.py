8af5cf4ee2760dc21c6acfff20aa207aefee0088
mdork <dorkmic@googlemail.com>
2020-06-19 13:27:58 +0200

    baseline

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np, os, sys, pandas as pd, ast, imp, time, gc
+import numpy as np, os, sys, pandas as pd, imp, ast, time, gc
 import torch, torch.nn as nn, pickle as pkl
 from PIL import Image
 from torchvision import transforms
@@ -6,107 +6,111 @@ from tqdm import tqdm, trange
 import network as net
 import auxiliaries as aux
 import argparse
+from distutils.dir_util import copy_tree
 from datetime import datetime
 
 
-"""=================================================================================="""
-"""=================================================================================="""
-def trainer(network, epoch, data_loader, Metrics, optimizer, loss_func):
+def trainer(network, epoch, data_loader, loss_track, optimizer, loss_func):
 
     _ = network.train()
-
-    start_time = time.time()
-
-    epoch_coll_acc, epoch_coll_loss  = [],[]
-
+    loss_track.reset()
+    logits_collect = []; labels_collect = []
     data_iter = tqdm(data_loader, position=2)
     inp_string = 'Epoch {} || Loss: --- | Acc: ---'.format(epoch)
     data_iter.set_description(inp_string)
 
     for image_idx, file_dict in enumerate(data_iter):
 
-        iter_start_time = time.time()
+        image = file_dict["image"].type(torch.FloatTensor).cuda()
+        label = file_dict["label"].type(torch.FloatTensor).cuda()
 
-        input_image     = torch.autograd.Variable(file_dict["Image"]).type(torch.FloatTensor).cuda()
-        target          = torch.autograd.Variable(file_dict["label"]).type(torch.FloatTensor).cuda()
-
-        #--- Run Training ---
-        prediction = network(input_image)
-
-        ### BASE LOSS
-        feed_dict = {'inp':prediction, 'target':target}
-        loss      = loss_func(**feed_dict)
+        logits = network(image)
+        loss      = loss_func(logits, label)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        prediction = np.argmax(logits.cpu().data.numpy(), axis=1)
+        acc = (np.round(prediction==label.cpu().data.numpy())).mean()
 
-        acc = (np.round(prediction.cpu().data.numpy())==target.cpu().data.numpy()).reshape(-1)
+        logits = nn.Sigmoid()(logits).detach().cpu() #TODO Use sigmoid?!
+        logits_collect.append(logits)
+        labels_collect.append(label.cpu().numpy())
 
-        #--- Get Scores ---
-        epoch_coll_acc.extend(list(acc))
-        epoch_coll_loss.append(loss.cpu().data.numpy()[0])
-
-        if image_idx%opt.Training['verbose_idx']==0 and image_idx:
-            inp_string = 'Epoch {} || Loss: {} | Acc: {}'.format(epoch,
-                                                                 np.round(np.mean(epoch_coll_loss),4),
-                                                                 np.round(np.sum(epoch_coll_acc)/len(epoch_coll_acc),4))
+        loss_dic = [loss.item(), acc]
+        loss_track.append(loss_dic)
+        if image_idx%20==0:
+            inp_string = 'Epoch {} || Loss: {} | Acc: {}'.format(epoch, np.round(loss.item(), 2), acc)
             data_iter.set_description(inp_string)
+
+    logits = torch.cat(logits_collect, dim=0)
+    label = np.concatenate(labels_collect, axis=0)
+    if logits.shape[1] > 1:
+        logits = nn.Softmax(dim=1)(logits).numpy()
+        pred  = np.sum(logits[:, 1:], axis=1)
+    else:
+        pred = logits.numpy().reshape(-1)
+
+    label[label>1] = 1
+    label = label.reshape(-1)
+    auc = aux.auc(label.astype(int), pred)
+    loss_track.append_auc(auc)
 
     ### Empty GPU cache
     torch.cuda.empty_cache()
+    loss_track.get_mean()
 
-    Metrics['Train Loss'].append(np.round(np.mean(epoch_coll_loss),4))
-    Metrics['Train Acc'].append(np.round(np.sum(epoch_coll_acc)/len(epoch_coll_acc),4))
 
-"""=================================================================================="""
-"""=================================================================================="""
-def validator(network, epoch, data_loader, Metrics, loss_func):
+def validator(network, epoch, data_loader, loss_track, loss_func):
 
     _ = network.eval()
+    loss_track.reset()
 
-    start_time = time.time()
-
-    epoch_coll_acc, epoch_coll_loss  = [],[]
-
+    logits_collect = []; labels_collect = []
     data_iter = tqdm(data_loader, position=2)
     inp_string = 'Epoch {} || Loss: --- | Acc: ---'.format(epoch)
     data_iter.set_description(inp_string)
 
-    for image_idx, file_dict in enumerate(data_iter):
+    with torch.no_grad():
+        for image_idx, file_dict in enumerate(data_iter):
 
-        iter_start_time = time.time()
+            image = file_dict["image"].type(torch.FloatTensor).cuda()
+            label = file_dict["label"].type(torch.FloatTensor).cuda()
 
-        input_image     = torch.autograd.Variable(file_dict["Image"]).type(torch.FloatTensor).cuda()
-        target          = torch.autograd.Variable(file_dict["label"]).type(torch.FloatTensor).cuda()
+            logits   = network(image)
+            loss     = loss_func(logits, label)
 
-        #--- Run Training ---
-        #NOTE: Test Triplet, Margin and BCE loss
-        prediction = network(input_image)
+            prediction = np.argmax(logits.cpu().data.numpy(), axis=1)
+            acc = (np.round(prediction==label.cpu().data.numpy())).mean()
 
-        ### BASE LOSS
-        feed_dict = {'inp':prediction, 'target':target}
-        loss      = loss_func(**feed_dict)
+            logits = nn.Sigmoid()(logits).detach().cpu()  # TODO Use sigmoid?!
+            logits_collect.append(logits)
+            labels_collect.append(label.cpu().numpy())
 
+            loss_dic = [loss.item(), acc]
 
-        acc = (np.round(prediction.cpu().data.numpy())==target.cpu().data.numpy()).reshape(-1)
+            loss_track.append(loss_dic)
+            if image_idx%20==0:
+                inp_string = 'Epoch {} || Loss: {} | Acc: {}'.format(epoch, np.round(loss.item(), 2), acc)
+                data_iter.set_description(inp_string)
 
-        #--- Get Scores ---
-        epoch_coll_acc.extend(list(acc))
-        epoch_coll_loss.append(loss.cpu().data.numpy()[0])
+    logits = torch.cat(logits_collect, dim=0)
+    label = np.concatenate(labels_collect, axis=0)
+    if logits.shape[1] > 1:
+        logits = nn.Softmax(dim=1)(logits).numpy()
+        pred  = np.sum(logits[:, 1:], axis=1)
+    else:
+        pred = logits.numpy().reshape(-1)
 
-        if image_idx%opt.Training['verbose_idx']==0 and image_idx:
-            inp_string = 'Epoch {} || Loss: {} | Acc: {}'.format(epoch,
-                                                                 np.round(np.mean(epoch_coll_loss),4),
-                                                                 np.round(np.sum(epoch_coll_acc)/len(epoch_coll_acc),4))
-            data_iter.set_description(inp_string)
+    label[label>1] = 1
+    label = label.reshape(-1)
+    auc = aux.auc(label.astype(int), pred)
+    loss_track.append_auc(auc)
 
     ### Empty GPU cache
     torch.cuda.empty_cache()
-
-    Metrics['Val Loss'].append(np.round(np.mean(epoch_coll_loss),4))
-    Metrics['Val Acc'].append(np.round(np.sum(epoch_coll_acc)/len(epoch_coll_acc),4))
+    loss_track.get_mean()
 
 
 """=================================================================================="""
@@ -115,127 +119,128 @@ def main(opt):
     """============================================"""
     ### Load Network
     imp.reload(net)
-    network = net.Extract_Scaffold(opt)
-    network.n_params = aux.gimme_params(network)
+    network = net.Network(opt.Network)
+    print("Number of parameters in model", sum(p.numel() for p in network.parameters()))
     _ = network.cuda()
 
     ### Set Optimizer
-    # Base_Loss   = aux.Base_Loss(weights=torch.autograd.Variable(torch.ones(opt.Network['n_classes'])))
-    loss_func   = aux.Base_Loss()
+    loss_func   = aux.Base_Loss(opt.Network)
     optimizer   = torch.optim.Adam(network.parameters(), lr=opt.Training['lr'], weight_decay=opt.Training['weight_decay'])
     scheduler   = torch.optim.lr_scheduler.StepLR(optimizer, step_size=opt.Training['step_size'], gamma=opt.Training['gamma'])
 
     """============================================"""
     ### Set Dataloader
     train_dataset     = aux.dataset(opt, mode='train')
-    train_data_loader = torch.utils.data.DataLoader(train_dataset, num_workers = opt.Training['kernels'],
-                                                    batch_size = opt.Training['bs'], shuffle=True)
-    val_dataset       = aux.dataset(opt, mode='val')
-    val_data_loader   = torch.utils.data.DataLoader(val_dataset,   num_workers = opt.Training['kernels'],
-                                                    batch_size = opt.Training['bs'], shuffle=False)
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, num_workers=opt.Training['workers'],
+                                                    batch_size=opt.Training['bs'], shuffle=True)
+    val_dataset       = aux.dataset(opt, mode='evaluation')
+    val_data_loader   = torch.utils.data.DataLoader(val_dataset, num_workers=opt.Training['workers'],
+                                                    batch_size=opt.Training['bs'], shuffle=False)
 
-
-    """============================================"""
-    ### Set Logging Files ###
+    """======================Set Logging Files======================"""
     dt = datetime.now()
     dt = '{}-{}-{}-{}-{}'.format(dt.year, dt.month, dt.day, dt.hour, dt.minute)
-    opt.Training['name'] = 'Extract-network_Iter-'+str(opt.iter_idx)+'_Date-'+dt
-    if opt.Training['savename']!="":
-        opt.Training['name'] += '_'+opt.Training['savename']
+    opt.Training['name'] = 'Model' + '_Date-' + dt  # +str(opt.iter_idx)+
+    if opt.Training['savename'] != "":
+        opt.Training['name'] += '_' + opt.Training['savename']
 
-    save_path = opt.Paths['save_path']+"/"+opt.Training['name']
+    save_path = opt.Paths['save_path'] + "/" + opt.Training['name']
 
-    #Make the saving directory
+    # Make the saving directory
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     else:
         count = 1
         while os.path.exists(save_path):
-            count        += 1
-            svn          = opt.name+"_"+str(count)
-            save_path    = opt.save_path+"/"+svn
-        opt.name = svn
+            count += 1
+            svn = opt.Training['name'] + "_" + str(count)
+            save_path = opt.Paths['save_path'] + "/" + svn
+        opt.Training['name'] = svn
         os.makedirs(save_path)
     opt.Paths['save_path'] = save_path
+
+    def make_folder(name):
+        if not os.path.exists(name):
+            os.makedirs(name)
+
+    # Make summary plots, images, segmentation and videos folder
+    save_summary = save_path + '/summary_plots'
+    make_folder(save_path + '/summary_plots')
+
+    ### Copy Code !!
+    copy_tree('./', save_path + '/code/')
 
     #Generate save string
     save_str = aux.gimme_save_string(opt)
 
     ### Save rudimentary info parameters to text-file and pkl.
-    with open(opt.Paths['save_path']+'/Parameter_Info.txt','w') as f:
+    with open(opt.Paths['save_path'] + '/Parameter_Info.txt', 'w') as f:
         f.write(save_str)
-    pkl.dump(opt,open(opt.Paths['save_path']+"/hypa.pkl","wb"))
+    pkl.dump(opt, open(opt.Paths['save_path'] + "/hypa.pkl", "wb"))
 
+    logging_keys = ["Loss", "ACC", "AUC"]
 
-    """============================================"""
-    logging_keys    = ["Train Acc", "Train Loss", "Val Acc", "Val Loss"]
-    Metrics         = {key:[] for key in logging_keys}
-    Timer           = {key:[] for key in ["Train","Val"]}
+    loss_track_train = aux.loss_tracking(logging_keys)
+    loss_track_test = aux.loss_tracking(logging_keys)
 
     ### Setting up CSV writers
-    full_log  = aux.CSVlogger(save_path+"/log_per_epoch.csv", ["Epoch", "Time", "Training Loss", "Training Acc", "Validation Acc", "Learning Rate"])
+    full_log_train = aux.CSVlogger(save_path + "/log_per_epoch_train.csv", ["Epoch", "Time", "LR"] + logging_keys)
+    full_log_test = aux.CSVlogger(save_path + "/log_per_epoch_test.csv", ["Epoch", "Time", "LR"] + logging_keys)
 
-
-    """============================================="""
-    epoch_iterator = tqdm(range(opt.Training['n_epochs']),position=1)
+    epoch_iterator = tqdm(range(0, opt.Training['n_epochs']), ascii=True, position=1)
     best_val_acc   = 0
 
     for epoch in epoch_iterator:
         epoch_time = time.time()
 
-        scheduler.step()
-
         ###### Training ########
-        epoch_iterator.set_description("Training with lr={}".format(np.round(scheduler.get_lr(),8)))
-        trainer(network, epoch, train_data_loader, Metrics, optimizer, loss_func)
+        epoch_iterator.set_description("Training with lr={}".format(np.round(scheduler.get_lr(), 6)))
+        trainer(network, epoch, train_data_loader, loss_track_train, optimizer, loss_func)
 
         ###### Validation #########
         epoch_iterator.set_description('Validating...')
-        validator(network, epoch, val_data_loader, Metrics, loss_func)
+        validator(network, epoch, val_data_loader, loss_track_test, loss_func)
 
 
         ###### SAVE CHECKPOINTS ########
-        save_dict = {'epoch': epoch+1, 'state_dict':network.state_dict(),
-                     'optim_state_dict':optimizer.state_dict()}
-        # Best Validation Score
-        if Metrics['Val Acc'][-1]>best_val_acc:
-            torch.save(save_dict, opt.Paths['save_path']+'/checkpoint_best_val.pth.tar')
-            best_val_acc = Metrics['Val Acc'][-1]
+        save_dict = {'epoch': epoch+1, 'state_dict': network.state_dict(),
+                     'optim_state_dict': optimizer.state_dict()}
 
-        # After Epoch
-        torch.save(save_dict, opt.Paths['save_path']+'/checkpoint.pth.tar')
+        # Best Validation Score
+        current_auc = loss_track_test.get_current_mean()[-1]
+        if current_auc > best_val_acc:
+            torch.save(save_dict, opt.Paths['save_path'] + '/checkpoint_best_val.pth.tar')
+            best_val_acc = current_auc
 
         ###### Logging Epoch Data ######
-        full_log.write([epoch, time.time()-epoch_time, np.mean(Metrics["Train Loss"][epoch]),np.mean(Metrics["Train Acc"][epoch]), np.mean(Metrics["Val Acc"][epoch]), scheduler.get_lr()[0]])
-
+        full_log_train.write([epoch, time.time() - epoch_time, [group['lr'] for group in optimizer.param_groups][0], *loss_track_train.get_current_mean()])
+        full_log_test.write([epoch, time.time() - epoch_time, [group['lr'] for group in optimizer.param_groups][0], *loss_track_test.get_current_mean()])
 
         ###### Generating Summary Plots #######
-        sum_title = 'Max Train Acc: {0:2.3f} | Max Val Acc: {1:2.3f}'.format(np.max([np.mean(Metrics["Train Acc"][ep]) for ep in range(epoch+1)]),
-                                                                             np.max([np.mean(Metrics["Val Acc"][ep]) for ep in range(epoch+1)]))
-        aux.progress_plotter(np.arange(epoch+1), \
-                             [np.mean(Metrics["Train Loss"][ep]) for ep in range(epoch+1)],[np.mean(Metrics["Train Acc"][ep]) for ep in range(epoch+1)],
-                             [np.mean(Metrics["Val Loss"][ep]) for ep in range(epoch+1)], [np.mean(Metrics["Val Acc"][ep]) for ep in range(epoch+1)], save_path+'/training_results.png', sum_title)
-
-
+        aux.summary_plots(loss_track_train.get_hist(), loss_track_test.get_hist(), epoch, save_summary)
+        scheduler.step()
         _ = gc.collect()
-
 
 
 ### Start Training ###
 if __name__ == '__main__':
-    """============================================"""
-    ### GET TRAINING SETUPs ###
-    #Read network and training setup from text file.
-    opt = argparse.Namespace()
-    opt.Paths = {}
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-cf", "--config", type=str, default='/export/home/mdorkenw/code/ALASKA2/network_base_setup.txt',
+                        help="Define config file")
+    args = parser.parse_args()
+    training_setups = aux.extract_setup_info(args.config)
 
-    ### DATA PATHS ###
-    opt.Paths['train_data']                   = '/home/karsten_dl/Dropbox/Data_Dump/HPA/train'
-    opt.Paths['assign_file']                  = '/home/karsten_dl/Dropbox/Data_Dump/HPA/train.csv'
-    opt.Paths['save_path']                    = '/home/karsten_dl/Dropbox/Data_Dump/HPA/Logs'
-    opt.Paths['network_base_setup_file']      = '/home/karsten_dl/Dropbox/Projects/current_projects/hpa-kaggle/Train/network_base_setup.txt'
-    opt.Paths['network_variation_setup_file'] = '/home/karsten_dl/Dropbox/Projects/current_projects/hpa-kaggle/Train/network_variation_setup.txt'
+    # find all the gpus we want to use now (currently not necessary, only if we want to run different
+    # training setting on different gpus at the same time)
+    gpus = []
+    for tr in training_setups:
+        for GPU in tr.Training['GPU']:
+            gpus.append(str(GPU))
 
-    training_setups = aux.extract_setup_info(opt)
-    for training_setup in tqdm(training_setups, desc='Training Setups... ', position=0):
+    gpus = ",".join(gpus)
+
+    # os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpus
+
+    for training_setup in tqdm(training_setups, desc='Training Setups... ', position=0, ascii=True):
         main(training_setup)
